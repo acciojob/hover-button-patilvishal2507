8c2b191ff90939d492ccbe6723225c4e254602c0
patilvishal2507 <117289866+patilvishal2507@users.noreply.github.com>
2024-09-26 21:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 //your code here
-#btn{
+#btn {
   background-color: black;
-  color:white ;
+  color: white;
   border-radius: 5px;
   border: none;
+  padding: 10px 20px;
+}
+
+#btn:hover {
+  background-color: yellow;
+  color: black;
+  border-radius: 30px;
 }
